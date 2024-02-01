e9872e9c48678ae909d1b0bdd7888f8c5b6f6e3e
sjtgs <chipilej@gmail.com>
2024-02-01 13:10:09 +0200

    Modified The Health Record System

--- health_system/health_system/settings.py
+++ health_system/health_system/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "Secret_Key"
+SECRET_KEY = ""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "doctor_app",
     "patient_app",
     "dashboard_app",
+    "insurance_app",
     "rest_framework",
 ]
 
