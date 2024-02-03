02d58cb10609e8d488737e906dfd45976f2454c8
sjtgs <chipilej@gmail.com>
2024-02-03 20:14:23 +0200

    Added Export Feature tho the Admin.py of the Doctor App

--- health_system/doctor_app/admin.py
+++ health_system/doctor_app/admin.py
@@ -1,5 +1,54 @@
 from django.contrib import admin
+from django.http import HttpResponse
+import csv
 from .models import Doctor, DoctorImage
+
+
+def export_doctors(modelsadmin, request, queryset):
+    response = HttpResponse(content="text/csv")
+    response["Content-Disposition"] = 'attachment; filename="doctor_records.csv"'
+
+    writer = csv.writer(response)
+    writer.writerow(
+        [
+            "ID",
+            "First Name",
+            "Last Name",
+            "Date of Birth",
+            "Gender",
+            "NRC",
+            "Province",
+            "Town",
+            "Address",
+            "Email",
+            "Phone Number",
+            "Created At",
+            "Updated At",
+        ]
+    )
+
+    for doctor in queryset:
+        writer.writerow(
+            [
+                doctor.auto_id,
+                doctor.first_name,
+                doctor.last_name,
+                doctor.date_of_birth,
+                doctor.gender,
+                doctor.nrc,
+                doctor.provinces,
+                doctor.towns,
+                doctor.address,
+                doctor.email,
+                doctor.phone_number,
+                doctor.created_at,
+                doctor.updated_at,
+            ]
+        )
+    return response
+
+
+export_doctors.short_description = "Export selected Doctors to CSV"
 
 
 @admin.register(Doctor)
@@ -35,6 +84,7 @@ class DoctorAdmin(admin.ModelAdmin):
         "created_at",
         "updated_at",
     )
+    actions = [export_doctors]
 
 
 @admin.register(DoctorImage)
