8adc81799b762ca9de76ba88a8844204431fd831
sjtgs <chipilej@gmail.com>
2024-02-22 20:30:45 +0200

    Added administration_app to the settings.py file

--- health_system/health_system/settings.py
+++ health_system/health_system/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "administration_app",
     "patient_app",
     "doctor_app",
     "dashboard_app",
