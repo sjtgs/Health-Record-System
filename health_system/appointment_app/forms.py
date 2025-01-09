9e1889f407b4cf2b9f678bc3f21e6ec3174b75c3
sjtgs <chipilej@gmail.com>
2025-01-09 10:58:05 +0200

    [WIP] Updated models for appointment app , added forms for the models and created utils for the ocr

--- health_system/appointment_app/forms.py
+++ health_system/appointment_app/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from appointment_app.models import Appointment
+from appointment_app.models import Appointment, PatientMedicalInfo , MedicalNotes
 
 
 # Created Appointment forms
@@ -7,3 +7,19 @@ class AppointmentForms(forms.ModelForm):
     class Meta:
         model = Appointment
         fields = ["date", "time", "reason"]
+
+# Created Patient Medical Information Form 
+class PatientMedicalInfoForms(forms.ModelForm):
+    class Meta:
+        model = PatientMedicalInfo
+        fields = ["patient", "weight", "height", "blood_pressure"]
+
+
+# Created Doctors Medical Notes 
+class MedicalNotesForm(forms.Form):
+    patient_id = forms.ModelChoiceField(
+        queryset=MedicalNotes.objects.all(),
+        to_field_name = "auto_id",
+        empty_label= "Select a Patient"
+    )
+    image = forms.ImageField()
--- health_system/appointment_app/models.py
+++ health_system/appointment_app/models.py
@@ -15,3 +15,26 @@ class Appointment(models.Model):
 
     def __str__(self):
         return f"The Appointment for {self.patient.first_name} {self.patient.last_name} {self.patient.date_of_birth} {self.patient.nrc} with Doctor {self.doctor.first_name} {self.doctor.last_name} on  {self.date} time {self.time} "
+    
+
+# Patient Information Appointment 
+class PatientMedicalInfo(models.Model):
+    patient =  models.ForeignKey(Patient, on_delete=models.CASCADE)
+    weight = models.IntegerField()
+    height = models.IntegerField()
+    blood_pressure = models.IntegerField()
+    
+
+    def __str__(self):
+        return f"Patient Medical Information {self.patient.first_name} {self.patient.last_name} "
+
+# Medical Notes from the Doctor and Patient 
+class MedicalNotes(models.Model):
+    doctor = models.ForeignKey(Doctor, on_delete=models.CASCADE)
+    patient_name = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    patient_info = models.ForeignKey(PatientMedicalInfo, on_delete=models.CASCADE)
+    doctor_notes = models.TextField()
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Doctor notes after Meeting with the Patient {self.patient_name.first_name} {self.patient_name.last_name} {self.patient_info.weight} {self.patient_info.height} {self.patient_info.blood_pressure} {self.doctor_notes} {self.doctor.last_name}"
